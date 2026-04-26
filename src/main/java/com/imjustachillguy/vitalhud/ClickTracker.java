6242082c25a65e598bbfc9dd74e578c912aa8daa
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-04-26 11:03:29 +0530

    Create ClickTracker.java

---
+++ src/main/java/com/imjustachillguy/vitalhud/ClickTracker.java
@@ -0,0 +1,33 @@
+package com.imjustachillguy.vitalhud.stats;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ClickTracker {
+
+    private static final ArrayList<Long> clicks = new ArrayList<>();
+
+    public static void init() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.mouse.wasLeftButtonClicked()) {
+                clicks.add(System.currentTimeMillis());
+            }
+
+            long now = System.currentTimeMillis();
+            Iterator<Long> it = clicks.iterator();
+
+            while (it.hasNext()) {
+                if (now - it.next() > 1000) {
+                    it.remove();
+                }
+            }
+        });
+    }
+
+    public static int getCPS() {
+        return clicks.size();
+    }
+}
