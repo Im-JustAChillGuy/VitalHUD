15f8193424bc8d62695d731bc8e019563874bfda
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-05-22 12:53:43 +0530

    Update VitalHUD.java

--- src/client/java/com/example/client/VitalHUD.java
+++ src/client/java/com/example/client/VitalHUD.java
@@ -1,13 +1,19 @@
 package com.example.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class VitalHUD implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+
         HudRenderer.init();
         ClickTracker.init();
         SpeedTracker.init();
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            HudRenderer.render();
+        });
     }
 }
