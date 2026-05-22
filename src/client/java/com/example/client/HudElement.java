3b9ea78fc270dd4d6094d569afeba8090731148a
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-05-22 13:03:11 +0530

    Create HudElement.java

---
+++ src/client/java/com/example/client/HudElement.java
@@ -0,0 +1,32 @@
+package com.example.client;
+
+public class HudElement {
+
+    private int x;
+    private int y;
+
+    private final String name;
+
+    public HudElement(String name, int x, int y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
