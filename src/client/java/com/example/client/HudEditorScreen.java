2a2b51a16da3e3e220bd3babd74eab17b131ec1c
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-05-22 13:19:37 +0530

    Update HudEditorScreen.java

--- src/client/java/com/example/client/HudEditorScreen.java
+++ src/client/java/com/example/client/HudEditorScreen.java
@@ -5,12 +5,27 @@ import net.minecraft.network.chat.Component;
 
 public class HudEditorScreen extends Screen {
 
-    protected HudEditorScreen() {
+    public HudEditorScreen() {
         super(Component.literal("VitalHUD Editor"));
     }
 
     @Override
-    public void renderBackground(net.minecraft.client.gui.GuiGraphics graphics, int mouseX, int mouseY, float delta) {
-        super.renderBackground(graphics, mouseX, mouseY, delta);
+    public void render(net.minecraft.client.gui.GuiDrawContext graphics, int mouseX, int mouseY, float delta) {
+
+        super.render(graphics, mouseX, mouseY, delta);
+
+        int x = 100;
+        int y = 100;
+
+        graphics.fill(x, y, x + 60, y + 12, 0x80000000);
+
+        graphics.drawString(
+            minecraft.font,
+            "FPS",
+            x + 2,
+            y + 2,
+            0xFFFFFF,
+            true
+        );
     }
 }
