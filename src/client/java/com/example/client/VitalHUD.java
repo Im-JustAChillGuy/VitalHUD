073fd54b881980869df1c73e863e042399289463
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-06-14 12:31:38 +0530

    Update VitalHUD.java

--- src/client/java/com/example/client/VitalHUD.java
+++ src/client/java/com/example/client/VitalHUD.java
@@ -1,34 +1,21 @@
 package com.example.client;
 
 import net.fabricmc.api.ClientModInitializer;
-
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-
-import net.minecraft.client.Minecraft;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class VitalHUD implements ClientModInitializer {
-
-    private boolean openedEditor = false;
 
     @Override
     public void onInitializeClient() {
 
         HudManager.init();
-
         HudRenderer.init();
         ClickTracker.init();
         SpeedTracker.init();
+        Keybinds.init();
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-
-            HudRenderer.render();
-
-            if (!openedEditor) {
-
-                Minecraft.getInstance().setScreen(new HudEditorScreen());
-
-                openedEditor = true;
-            }
+        HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
+            HudRenderer.render(drawContext);
         });
     }
 }
