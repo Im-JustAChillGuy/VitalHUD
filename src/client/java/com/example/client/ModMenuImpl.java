229f611b190376f9873d901996c06ec594865589
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-06-22 17:31:41 +0530

    Create ModMenuImpl.java

---
+++ src/client/java/com/example/client/ModMenuImpl.java
@@ -0,0 +1,12 @@
+package com.example.nebula;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuImpl implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return NebulaConfigScreen::new;
+    }
+}
